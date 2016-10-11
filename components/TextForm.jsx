67306e1165c36a2080199b518b89fbb7619bedc3
Fujimisakari <fujimisakari@gmail.com>
2016-10-12 02:08:39 +0900

    Improve textform

--- components/TextForm.jsx
+++ components/TextForm.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 
 import TextField from 'material-ui/TextField';
 
-const TextForm = ({ hintText, name, onChange }) => {
+const TextForm = ({ hintText, name, value, onChange }) => {
   const enableFullWidth = true;
   return (
     <div>
       <TextField
         hintText={hintText}
         name={name}
+        value={value}
         onChange={onChange}
         fullWidth={enableFullWidth}
       />
