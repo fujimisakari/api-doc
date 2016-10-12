d899e61b50064fa6318019b829032e2b2c9544c2
Fujimisakari <fujimisakari@gmail.com>
2016-10-12 09:28:57 +0900

    Fix testerResponse reducer

--- reducers/testerResponse.jsx
+++ reducers/testerResponse.jsx
@@ -1,9 +1,11 @@
-import { TESTER_RESPONSE } from '../constants';
+import { TESTER_RESPONSE, UPDATE_FORM } from '../constants';
 
 export default function testerResponse(state = { requestURL: '', data: {} }, action) {
   switch (action.type) {
     case TESTER_RESPONSE:
       return Object.assign({}, state, { requestURL: action.requestURL, data: action.resoponseData });
+    case UPDATE_FORM:
+      return state;
     default:
       return { requestURL: '', data: {} };
   }
